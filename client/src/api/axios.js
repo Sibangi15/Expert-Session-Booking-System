a33f46805c828351c92021cfc7f13ee123b28563
Sibangi15 <sibangichakraborty2005@gmail.com>
2026-05-09 15:59:53 +0530

    Backend URL Updated

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://expert-session-booking-system-0gmy.onrender.com/api"
 });
